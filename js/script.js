cc87d4c830babdd72f3f50b3e32e38fe24f2866c
Ivan Sempebwa <iproivan@gmail.com>
2024-11-20 19:33:12 +0300

    ronald

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 'use strict'
 
 const members = [
-    'VU-BCS-2201-0378-EVE', 'VU-BCS-2201-0003-EVE', 'VU-BCS-2201-1542-EVE', 'VU-BCS-2201-0123-EVE', 
+    'VU-BCS-2201-0378-EVE', 'VU-BCS-2201-0003-EVE', 'VU-BCS-2201-1542-EVE', 'VU-BCS-2201-0123-EVE',
     'VU-BIT-2407-0188-EVE', 'VU-DIT-2403-1299-EVE', 'VU-BIT-2407-0958-EVE', 'VU-DAV-2403-0885-EVE',
-    'VU-BIT-2403-0852-EVE', 'VU-BIT-2403-0384-EVE'
+    'VU-BIT-2403-0852-EVE', 'VU-BIT-2403-0384-EVE', 'VU-BIT-2201-1038-EVE'
 ]
 
 const navigation = document.querySelector('.navigation')
 
-members.forEach((member,i) => {
+members.forEach((member, i) => {
     const link = document.createElement('a')
     link.href = `${member}.html`
-    link.innerText = i+1
+    link.innerText = i + 1
 
     navigation.appendChild(link)
 })
@@ -44,7 +44,7 @@ const message = new SpeechSynthesisUtterance()
 const readBtn = document.getElementById('read').addEventListener('click', e => {
     const main = document.querySelector('main')
     message.text = main.innerText
-    
+
     speechSynthesis.speak(message)
 })
 
@@ -60,12 +60,12 @@ document.getElementById('resume').addEventListener('click', e => {
 document.getElementById('phone-input').addEventListener('input', (e) => {
     let value = e.target.value.replace(/\D/g, "")
 
-    if (value.length > 12) value = value.substring(0,12)
-    
+    if (value.length > 12) value = value.substring(0, 12)
+
     // +(256) 770-506234
-    const areaCode = value.substring(0,3)
+    const areaCode = value.substring(0, 3)
     const carrierPart = value.substring(3, 6)
-    const digitsPart = value.substring(6,12)
+    const digitsPart = value.substring(6, 12)
 
     let formatedPhone = ''
     if (areaCode) {
